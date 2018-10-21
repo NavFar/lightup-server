85ed1b67b3a87d9257558dcdf48aff986eb07435
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-22 00:03:11 +0330

    add route mechanism

---
+++ configs/router.js
@@ -0,0 +1,8 @@
+var router = {
+ controllers : "../controllers/",
+add:function(app){
+  // app.use('/',controllers+'');
+
+}
+};
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-// var router = require('./configs/router');
+var router = require('./configs/router');
 var dbConfig = require('./configs/dbConfig');
 var mongoose = require('mongoose');
 // mongoose.connect('mongodb://'+dbConfig.user+':'+dbConfig.db_passowrd+'@'+dbConfig.db_url+':'+dbConfig.db_port+'/'+dbConfig.db_name);
 mongoose.connect('mongodb://'+dbConfig.db_url+':'+dbConfig.db_port+'/'+dbConfig.db_name,{ useNewUrlParser: true });
-// router.add(app);
+router.add(app);
 var portNumber = 3000;
 app.listen(portNumber,function()
 {
