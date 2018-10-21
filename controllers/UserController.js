5ae8cca48bf9a88800ea2e6adb8d7d932d32009e
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-22 00:52:20 +0330

    add self route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,12 @@ var User = require('../models/User')
 var authConfig = require('../configs/authConfig');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-
+//  ____            _     _
+// |  _ \ ___  __ _(_)___| |_ ___ _ __
+// | |_) / _ \/ _` | / __| __/ _ \ '__|
+// |  _ <  __/ (_| | \__ \ ||  __/ |
+// |_| \_\___|\__, |_|___/\__\___|_|
+//           |___/
 router.post('/register', function(req, res) {
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
   User.create({
@@ -19,6 +24,21 @@ router.post('/register', function(req, res) {
       expiresIn: 86400
     });
     res.status(200).send({ token: token });
+  });
+});
+//           _  __
+//  ___  ___| |/ _|
+// / __|/ _ \ | |_
+// \__ \  __/ |  _|
+// |___/\___|_|_|
+router.get('/self', function(req, res) {
+  var token = req.headers['x-access-token'];
+  if (!token) return res.status(401).send({message: 'No token provided.' });
+
+  jwt.verify(token, authConfig.secret, function(err, decoded) {
+    if (err) return res.status(500).send({ message: 'Failed to authenticate token.' });
+
+    res.status(200).send(decoded);
   });
 });
 
