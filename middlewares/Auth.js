73751fb35e1588f6c546177a1d4999dab028abbf
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-22 03:45:53 +0330

    middleware now just give user and nothing else

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -8,8 +8,6 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var authConfig = require('../configs/authConfig');
 var token = req.headers['x-access-token'];
-res.locals.auth=false;
-res.locals.user=null;
 if(token)
 {
   jwt.verify(token, authConfig.secret, function(err, decoded) {
@@ -18,7 +16,6 @@ if(token)
       User.findById(decoded.id, function (err, user) {
         if(!err&&user)
         {
-          res.locals.auth=true;
           res.locals.user=user;
         }
           next();
