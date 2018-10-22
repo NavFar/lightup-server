7f5c554f5a1a640b099ead54f90bdbf62dbc60e1
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-22 14:54:59 +0330

    add log controller to router

--- configs/router.js
+++ configs/router.js
@@ -2,6 +2,7 @@ var router = {
  controllers : "../controllers/",
 add:function(app){
   app.use('/user',require(this.controllers+'UserController'));
+  app.use('/log',require(this.controllers+'LogController'));
 }
 };
 module.exports = router;
