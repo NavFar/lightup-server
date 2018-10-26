92a09819be268e75c10d364c43d65197a2cee033
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-26 23:09:08 +0330

    add pattern route and prefix to all

--- configs/router.js
+++ configs/router.js
@@ -1,6 +1,6 @@
 var router = {
  controllers : "../controllers/",
- prefix : "api/"
+ prefix : "/api",
 add:function(app){
   app.use(this.prefix+'/user',require(this.controllers+'UserController'));
   app.use(this.prefix+'/log',require(this.controllers+'LogController'));
