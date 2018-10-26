0dfc9af980c0046ae643e498def7d31fde7a2504
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-26 17:22:01 +0330

    add room controller

--- configs/router.js
+++ configs/router.js
@@ -3,6 +3,7 @@ var router = {
 add:function(app){
   app.use('/user',require(this.controllers+'UserController'));
   app.use('/log',require(this.controllers+'LogController'));
+  app.use('/room',require(this.controllers+'RoomController'));
 }
 };
 module.exports = router;
