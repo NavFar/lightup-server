6baeed98c228c7211157ed711039a935b9699186
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-28 04:34:27 +0330

    add public to server

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app = express();
 var router = require('./configs/router');
 var dbConfig = require('./configs/dbConfig');
 var mongoose = require('mongoose');
+var path = require('path');
 mongoose.set('useFindAndModify', false);
 // mongoose.connect('mongodb://'+dbConfig.user+':'+dbConfig.db_passowrd+'@'+dbConfig.db_url+':'+dbConfig.db_port+'/'+dbConfig.db_name);
 mongoose.connect('mongodb://'+dbConfig.db_url+':'+dbConfig.db_port+'/'+dbConfig.db_name,{ useNewUrlParser: true });
 app.all('*',require('./middlewares/Auth'));
+app.use(express.static(path.join(__dirname, 'public')));
 router.add(app);
 var portNumber = 3000;
 app.listen(portNumber,function()
